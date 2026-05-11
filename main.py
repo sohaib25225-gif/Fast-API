a5bab0e5985cda8aeb13b6185718c1442f968787
sohaib  25225 <Sohaib25225@gmail.com>
2026-05-11 11:21:22 +0500

    updated main.py again

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 app=FastAPI()
 
-@app.get("/")
-def index():
-    return {"data":"blog list"}
+@app.get("/blog") 
+def index(limit):
+    # only get 10 published blogs
+    return {"data":f"{limit}blogs from the db"}
 
+
+@app.get("/blog/unpublished")
+def unpublished():
+    # fetch unpublished blogs
+    return {"data":"all unpublished blogs"}
 
 @app.get("/blog/{id}") 
-def show(id):
+def show(id:int):
     # fetch blog with id = id
     return {"data":id}
- 
+
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    # fetch comments of blog with id = id
+    return {"data":{'1','2'}}
