991e730c4e2f0f6621eb67494bd2ca357815308a
sohaib  25225 <Sohaib25225@gmail.com>
2026-05-12 14:47:21 +0500

    updated main.py with changing port

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import uvicorn
 
 app=FastAPI()
 
@@ -37,3 +38,7 @@ class Blog(BaseModel):
 @app.post("/blog")
 def create_blog(blog:Blog):
     return {"data":f"Blog is created with title as {blog.title} and body as {blog.body}"}
+
+# if __name__=="__main__":
+#     uvicorn.run(app,host="127.0.0.1",port=9000)
+    
